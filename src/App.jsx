a101558ed0641488f5642f02dc4284cbf16338b5
André Knob <dextra.aafonso@creditas.com>
2022-10-23 23:14:43 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+const MUTATION_OBSERVE_OPTIONS = {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true,
+};
+
+function App() {
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    const mutationObserver = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        console.log("mutation:");
+        console.log(mutation);
+      });
+    });
+
+    mutationObserver.observe(
+      document.documentElement,
+      MUTATION_OBSERVE_OPTIONS
+    );
+
+    return () => mutationObserver.disconnect();
+  }, []);
+
+  return (
+    <div>
+      <input
+        type="button"
+        value="button"
+        onClick={() => setShowText(true)}
+      ></input>
+      <div style={{ marginTop: "64px" }}>{showText ? "react test" : false}</div>
+    </div>
+  );
+}
+
+export default App;
